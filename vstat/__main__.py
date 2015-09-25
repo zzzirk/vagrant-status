724ccc936e2056894cb5f36b123ebc94fe86c676
Lou Zirkel <louis.zirkel@dishdigital.com>
2015-09-25 12:42:30 -0600

    Removed all the old cruft to clean up the main module.

--- vstat/__main__.py
+++ vstat/__main__.py
@@ -1,34 +1,6 @@
-import sys
 import subprocess
 from collections import defaultdict
-from pprint import pprint
-from .vstat import StatusEntry
 from .vstat import VagrantStatus
-
-
-def old_main():
-
-    vp = subprocess.Popen(["vagrant", "global-status"], stdout=subprocess.PIPE)
-    result = vp.stdout.read().split('\n')
-    # print result
-    stats = {}
-    for r in result[2:]:
-        if r == ' ':
-            break
-        e = StatusEntry.from_text(r)
-        # m = RE_statline.match(r)
-        # if m:
-        #     e = StatusEntry.from_text(*m.groups())
-        if e.project not in stats:
-            stats[e.project] = []
-        stats[e.project].append(e)
-
-    for key, value in stats.iteritems():
-        nextkey = key
-        for v in value:
-            print("{:16}{:10}{}".format(nextkey, v, v.state))
-            if nextkey == key:
-                nextkey = ""
 
 
 def main():
@@ -37,9 +9,6 @@ def main():
     stats = defaultdict(list)
     for status in VagrantStatus(vp.stdout):
         stats[status.project].append(status)
-
-    # pprint(stats)
-    # sys.exit(0)
 
     for key in sorted(stats.keys()):
         value = stats[key]
