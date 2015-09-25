2027abbf1e5439df6a8dd4565a4b805d66c468f3
Lou Zirkel <louis.zirkel@dishdigital.com>
2015-09-25 12:45:12 -0600

    Removed old StatusEntry class and other cruft.

--- vstat/vstat.py
+++ vstat/vstat.py
@@ -1,9 +1,5 @@
-from collections import namedtuple, defaultdict
+from collections import defaultdict
 import os
-
-
-StatusTuple = namedtuple('StatusTyple',
-                         'token, hostname, provider, state, path')
 
 
 class VagrantStatusEntry(object):
@@ -61,44 +57,3 @@ class VagrantStatus(object):
                 end = self.format.offset[key]['end']
                 args.append(line[start:end].strip())
             return VagrantStatusEntry(*args)
-
-
-class StatusEntry(object):
-    """
-    A basic representation of a status line from running:
-
-        $ vagrant global-status
-
-    """
-    def __init__(self, token, hostname, provider, state, path):
-        self.token = token.strip()
-        self.hostname = hostname.strip()
-        self.provider = provider.strip()
-        self.state = state.strip()
-        self.path = path.strip()
-
-    @property
-    def project(self):
-        return os.path.split(self.path)[1]
-
-    def __repr__(self):
-        return "<NewStatsEntry: {}>".format(self.token)
-
-    def __str__(self):
-        return "{}".format(self.token)
-
-    @staticmethod
-    def from_text(line):
-        # RE_statline = re.compile("^(?P<token>.*?)\s+(?P<hostname>.*?)\s+"
-        #                          "(?P<provider>.*?)\s+(?P<state>.*?)\s+"
-        #                          "(?P<path>.*?)$")
-        # m = RE_statline.match(r)
-        # if m:
-        #     e = StatusEntry.from_text(*m.groups())
-        #     return e
-        token = line[0:8].strip()
-        hostname = line[8:15].strip()
-        provider = line[15:29].strip()
-        state = line[29:40].strip()
-        path = line[40:].strip()
-        return StatusEntry(token, hostname, provider, state, path)
