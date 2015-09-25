62ca650e04b7d8a0ac3e29046b5336254375ba15
Lou Zirkel <louis.zirkel@dishdigital.com>
2015-09-25 12:41:37 -0600

    Changed to using the new classes which better break things out.

--- vstat/__main__.py
+++ vstat/__main__.py
@@ -1,13 +1,12 @@
+import sys
 import subprocess
-import re
+from collections import defaultdict
+from pprint import pprint
 from .vstat import StatusEntry
+from .vstat import VagrantStatus
 
 
-def main():
-
-    RE_statline = re.compile("^(?P<token>.*?)\s+(?P<hostname>.*?)\s+"
-                             "(?P<provider>.*?)\s+(?P<state>.*?)\s+"
-                             "(?P<path>.*?)$")
+def old_main():
 
     vp = subprocess.Popen(["vagrant", "global-status"], stdout=subprocess.PIPE)
     result = vp.stdout.read().split('\n')
@@ -16,14 +15,34 @@ def main():
     for r in result[2:]:
         if r == ' ':
             break
-        m = RE_statline.match(r)
-        if m:
-            e = StatusEntry(*m.groups())
+        e = StatusEntry.from_text(r)
+        # m = RE_statline.match(r)
+        # if m:
+        #     e = StatusEntry.from_text(*m.groups())
         if e.project not in stats:
             stats[e.project] = []
         stats[e.project].append(e)
 
     for key, value in stats.iteritems():
+        nextkey = key
+        for v in value:
+            print("{:16}{:10}{}".format(nextkey, v, v.state))
+            if nextkey == key:
+                nextkey = ""
+
+
+def main():
+    vp = subprocess.Popen(["vagrant", "global-status"], stdout=subprocess.PIPE)
+
+    stats = defaultdict(list)
+    for status in VagrantStatus(vp.stdout):
+        stats[status.project].append(status)
+
+    # pprint(stats)
+    # sys.exit(0)
+
+    for key in sorted(stats.keys()):
+        value = stats[key]
         nextkey = key
         for v in value:
             print("{:16}{:10}{}".format(nextkey, v, v.state))
